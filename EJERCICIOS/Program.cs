fc8c81985b49ab19bb9d5bceb2493da0a8831f00
fstrings <alavajaime21@gmail.com>
2024-07-08 20:00:38 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIOS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIOS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("========  CALCULADORA  ========");
+            Console.WriteLine("\n Ingrese primer numero: ");
+            string num1 = Console.ReadLine();
+            num1 = num1.
+            Console.WriteLine("\n Ingrese segundo valor: ");
+            string num2 = Console.ReadLine();
+
+            float suma = num1 + num2;
+            float resta = num1 - num2;
+            float mult = num1 * num2;
+            float div = num1 / num2;
+
+            Console.WriteLine($"La suma de los numeros es {suma}");
+            Console.WriteLine($"La suma de los numeros es {resta}");
+            Console.WriteLine($"La suma de los numeros es {mult}");
+            Console.WriteLine($"La suma de los numeros es {div}");
+
+
+
+
+        }
+    }
+}
---
+++ Ejemplo proyecto/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_proyecto
+{//LLAVE INICIO SOLUCION
+    //namespace encapsula la clase para evitar confictos con demas proyectos
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//llave de inicio del main
+
+            //TIPOS DE DATOS
+
+            /*
+            * Comentario multilinea
+            */
+
+         
+            //para declarar una variable primero indico el tipo de dato
+
+            //tipo de datos: entero   
+            
+            int numero = 4;
+
+            //para imprimir utilizo la clase Console con el método .WriteLine()
+
+            Console.WriteLine(numero);
+            Console.WriteLine("el número ingresado es: " + numero);
+            Console.WriteLine($"el número ingresado es {numero}");
+            Console.ReadKey();
+            //Si declaro una variable de tipo byte solo puedo almacenar un numero entre 0 y 255
+            byte numero1 = 255;
+            Console.WriteLine(numero1);
+
+            //Un short, que puede almacenar valores de -32768 a 32767
+            short numero2 = 45;
+
+            //Un long, que puede almacenar valores de -9223372036854775808 a 9223372036854775807.
+            long numero3 = 67;
+
+            //Tipos de datos: Flotantes
+            float numero4 = 5.4f;
+            double numero5 = 5.456987d;
+
+            //tipo cadenas
+            char caracter = 'a';
+            string nombre3 = "Jaime";
+
+            //De tipo Bool
+            bool esLunes = true;
+            bool esMartes = false;
+
+            //Operadores aritméticos
+            //+,-,*,/,%
+
+            //Operadores Lógicos
+            // &&, ||, ,!
+
+            //Operadores de Incremento
+            //++, --
+
+            //Entrada de datos por teclado
+
+            Console.WriteLine("Ingrese su nombre: ");
+            string nombre1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad: ");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine($"Hola {nombre1} tu edad es {edad}");
+            Console.ReadLine();
+
+            
+
+
+
+        }//llave de cierre método main
+    }
+}//LLAVE DE CIERRE SOLUCION
